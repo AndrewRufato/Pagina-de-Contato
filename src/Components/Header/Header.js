aa082c7ef5a4f17b670301deb2f6621dc904b4af
Andrew Rufato <87341104+AndrewRufato@users.noreply.github.com>
2024-10-02 20:32:51 -0300

    First Commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+function Header (){
+    <header>
+        <nav> 
+            <a class='nav-item' href="#">Contato</a>
+            <a class='nav-item' href="#">Linkedinn</a>
+            <a class='nav-item' href="#">Currículo</a>
+        </nav>
+    </header>
+}
+
+export default Header
