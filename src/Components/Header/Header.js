cbc8387572e4fa8f06ab337d7e24f6777e21d2c8
Andrew Rufato <87341104+AndrewRufato@users.noreply.github.com>
2024-10-03 13:13:42 -0300

    Sucesso no uso do CDN do google fonts, Link adicionado para meu linkedin.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 function Header (){
+    return(
     <header>
         <nav> 
             <a class='nav-item' href="#">Contato</a>
-            <a class='nav-item' href="#">Linkedinn</a>
+            <a class='nav-item' href="https://www.linkedin.com/in/andrew-rufato-845406211/" target="_blank">Linkedinn</a>
             <a class='nav-item' href="#">Currículo</a>
         </nav>
     </header>
+    )
 }
 
 export default Header
